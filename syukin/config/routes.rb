cae22d271bf97c24a406602dca024b17bf90e085
Hiroki Aika <hirohiroaika@outlook.jp>
2020-02-26 17:13:55 +0900

    resources追加

--- syukin/config/routes.rb
+++ syukin/config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
-  get 'moneys/index'
-  get 'moneys/create'
-  get 'moneys/new'
-  get 'moneys/update'
-  get 'moneys/edit'
-  get 'moneys/destroy'
-  get 'moneys/show'
-  get 'top/index'
+  resources :moneys
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'top#index'
 end
