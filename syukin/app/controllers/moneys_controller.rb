877660edacbb09e81d8335a140860503b6e2b36b
Hiroki Aika <hirohiroaika@outlook.jp>
2020-02-28 12:06:10 +0900

    コミット

--- syukin/app/controllers/moneys_controller.rb
+++ syukin/app/controllers/moneys_controller.rb
@@ -1,6 +1,7 @@
 class MoneysController < ApplicationController
   def index
     @moneys = Money.all
+    @moneys_arr = @moneys.pluck(:deposit)
     @sum_money = Money.all.sum(:extra_money)
   end
 
